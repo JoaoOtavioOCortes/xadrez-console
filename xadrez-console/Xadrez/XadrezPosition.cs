2a47e165ed72a257c5751fcdfafe598bc4635734
Joao Otavio <ojao.bolinha@gmail.com>
2022-09-15 10:42:00 -0300

    Creating xadrezposition class

---
+++ xadrez-console/Xadrez/XadrezPosition.cs
@@ -0,0 +1,31 @@
+﻿using board;
+
+namespace Xadrez
+{
+    internal class XadrezPosition
+    {
+        public char Column { get; set; }
+        public int Line { get; set; }
+
+
+        public XadrezPosition()
+        {
+        }
+
+        public XadrezPosition(char column, int line)
+        {
+            Column = column;
+            Line = line;
+        }
+
+        public Position ToPosition()
+        {
+            return new Position(8 - Line, Column - 'a');
+        } 
+
+        public override string ToString()
+        {
+            return "" + Column + Line;
+        }
+    }
+}
